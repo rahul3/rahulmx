a53705483355c0d7871e6776a1511a23d697b2fa
Rahul Padmanabhan <rahul3@mail.usf.edu>
2021-09-18 21:21:13 -0400

    disabled hamburger menu

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
 st.title('rahul.mx : Work in progress')
+
+hide_menu_style = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        </style>
+        """
+
+st.markdown(hide_menu_style, unsafe_allow_html=True)
